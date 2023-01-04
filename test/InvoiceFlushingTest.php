5a72a702888be29ba6a7147953464209b0422b97
Harray <gong86627@qq.com>
2023-01-04 14:42:55 +0800

    全电发票通过流水号全额冲红

--- test/InvoiceFlushingTest.php
+++ test/InvoiceFlushingTest.php
@@ -22,6 +22,10 @@ class InvoiceFlushingTest extends TestCase
         $client = Client::newClient(Config::ClientId, Config::ClientSecret, Config::EncryptKey);
         $client->setToken(Config::AccessToken);
         $req = new InvoiceFlushingReq();
+        $req->serialNo = "";  //发票序列号 必填
+        $req->originalSerialNo = "";  //指定生成红票流水号 非必填
+        $req->redReason = 2;  //红冲原因 非必填
+
         $res = InvoiceFlushing::req($client, $req);
         var_dump($res);
         $this->assertEquals('0000', $res->errcode);
