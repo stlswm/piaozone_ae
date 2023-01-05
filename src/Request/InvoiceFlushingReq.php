82c8705083bb64bfc62afc11e76c671f8dcd4a7a
stlswm <691207572@qq.com>
2023-01-05 14:37:27 +0800

    开票接口、发票查询接口

--- src/Request/InvoiceFlushingReq.php
+++ src/Request/InvoiceFlushingReq.php
@@ -22,5 +22,5 @@ class InvoiceFlushingReq extends Req
      * 红冲原因  不填系统默认传2，需要税局校验 1:销货退回 2:开票有误 3:服务中止 4:销售折让 （此参数非必填）
      * @var string
      */
-    public string $redReason;
+    public string $redReason = '1';
 }
